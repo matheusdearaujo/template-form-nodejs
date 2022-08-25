386872c3cbf6e5120495d809f2d91e0bb812e12c
matheusdearaujo <matheusouzadearaujo@gmail.com>
2022-08-24 21:14:51 -0300

    Corrections

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,119 +1,127 @@
 document.addEventListener("DOMContentLoaded", () => {
-  navScrolling();
-  toggleButton();
-  activeLink();
-  formSubmit();
+	navScrolling();
+	toggleButton();
+	activeLink();
+	formSubmit();
 });
 
 const navScrolling = () => {
-  let nav = document.querySelector("nav");
+	const nav = document.querySelector("nav");
 
-  window.addEventListener("scroll", (e) => {
-    if (window.pageYOffset > 100) {
-      nav.classList.add("nav-scrolling");
-    } else {
-      nav.classList.remove("nav-scrolling");
-    }
-  });
+	window.addEventListener("scroll", () => {
+		if (window.pageYOffset > 100) nav.classList.add("nav-scrolling");
+		else nav.classList.remove("nav-scrolling");
+	});
 };
 
 const toggleButton = () => {
-  const icon = document.querySelector(".toggle");
+	const icon = document.querySelector(".toggle");
+	const nav = document.querySelector("nav");
+	const navLink = document.querySelector(".nav-link-mobile");
 
-  icon.addEventListener("click", (e) => {
-    document
-      .querySelectorAll(".nav-menu-mobile")
-      .forEach((nav) => nav.classList.toggle("toggle-show"));
+	nav.addEventListener("click", () => {
+		document
+			.querySelectorAll(".nav-menu-mobile")
+			.forEach(nav => nav.classList.toggle("toggle-show"));
 
-    if (icon.classList.contains("teste")) {
-      icon.innerHTML = '<i class="fa fa-angle-down"></i>';
-      icon.classList.remove("teste");
-    } else {
-      icon.innerHTML = '<i class="fa fa-angle-up"> </i>';
-      icon.classList.add("teste");
-    }
-  });
+		if (icon.classList.contains("toggle-active")) {
+			icon.innerHTML = '<i class="fa fa-angle-down"></i>';
+			icon.classList.remove("toggle-active");
+		} else {
+			icon.innerHTML = '<i class="fa fa-angle-up"> </i>';
+			icon.classList.add("toggle-active");
+		}
+	});
+
+	navLink.addEventListener("click", () => {
+		document
+			.querySelectorAll(".nav-menu-mobile")
+			.forEach(nav => nav.classList.remove("toggle-show"));
+	});
 };
 
 const activeLink = () => {
-  const sections = document.querySelectorAll("section[id]");
+	const sections = document.querySelectorAll("section[id]");
 
-  function scrollActive() {
-    const scrollY = window.pageYOffset;
+	const scrollActive = () => {
+		const scrollY = window.pageYOffset;
 
-    sections.forEach((current) => {
-      const sectionHeight = current.offsetHeight;
-      const sectionTop = current.offsetTop - 300;
+		sections.forEach(current => {
+			const sectionHeight = current.offsetHeight;
+			const sectionTop = current.offsetTop - 300;
 
-      sectionId = current.getAttribute("id");
+			sectionId = current.getAttribute("id");
 
-      if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-        document
-          .querySelector(".nav-menu a[href*=" + sectionId + "]")
-          .classList.add("active-link");
-      } else {
-        document
-          .querySelector(".nav-menu a[href*=" + sectionId + "]")
-          .classList.remove("active-link");
-      }
-    });
-  }
+			if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight)
+				document
+					.querySelector(".nav-menu a[href*=" + sectionId + "]")
+					.classList.add("active-link");
+			else
+				document
+					.querySelector(".nav-menu a[href*=" + sectionId + "]")
+					.classList.remove("active-link");
+		});
+	};
 
-  window.addEventListener("scroll", scrollActive);
+	window.addEventListener("scroll", scrollActive);
 };
 
 const formSubmit = () => {
-  const form = document.querySelector("#contactForm");
-  const button = document.querySelector("#btnForm");
-  let name = document.getElementById("contactName");
-  let subject = document.getElementById("contactSubject");
-  let email = document.getElementById("contactEmail");
-  let message = document.getElementById("contactMessage");
+	const form = document.querySelector("#contactForm");
+	const button = document.querySelector("#btnForm");
+	const name = document.getElementById("contactName");
+	const subject = document.getElementById("contactSubject");
+	const email = document.getElementById("contactEmail");
+	const message = document.getElementById("contactMessage");
 
-  const Toast = Swal.mixin({
-    toast: true,
-    position: "top-end",
-    showConfirmButton: false,
-    timer: 3000,
-    timerProgressBar: true,
-  });
+	const Toast = Swal.mixin({
+		toast: true,
+		position: "top-end",
+		showConfirmButton: false,
+		timer: 3000,
+		iconColor: "white",
+		customClass: {
+			popup: "colored-toast",
+		},
+	});
 
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
+	form.addEventListener("submit", e => {
+		e.preventDefault();
 
-    button.innerHTML = '<i class="fas fa-circle-notch fa-spin"></i>';
+		button.innerHTML = '<i class="fas fa-circle-notch fa-spin"></i>';
 
-    let formData = {
-      name: name.value,
-      subject: subject.value,
-      email: email.value,
-      message: message.value,
-    };
+		const formData = {
+			name: name.value,
+			subject: subject.value,
+			email: email.value,
+			message: message.value,
+		};
 
-    let xhr = new XMLHttpRequest();
-    xhr.open("POST", "/");
-    xhr.setRequestHeader("content-type", "application/json");
-    xhr.onload = function () {
-      button.innerHTML = '<i class="fas fa-paper-plane"></i>';
+		const xhr = new XMLHttpRequest();
 
-      if (xhr.status == 200) {
-        Toast.fire({
-          icon: "success",
-          title: "Email enviado!",
-        });
+		xhr.open("POST", "/");
+		xhr.setRequestHeader("content-type", "application/json");
+		xhr.onload = function () {
+			button.innerHTML = '<i class="fas fa-paper-plane"></i>';
 
-        name.value = "";
-        subject.value = "";
-        email.value = "";
-        message.value = "";
-      } else {
-        Toast.fire({
-          icon: "error",
-          title: "Ocorreu um erro, tente novamente mais tarde.",
-        });
-      }
-    };
+			if (xhr.status == 200) {
+				Toast.fire({
+					icon: "success",
+					title: "Email enviado!",
+				});
 
-    xhr.send(JSON.stringify(formData));
-  });
+				name.value = "";
+				subject.value = "";
+				email.value = "";
+				message.value = "";
+			} else {
+				Toast.fire({
+					icon: "error",
+					title: "Ocorreu um erro, tente novamente mais tarde.",
+				});
+			}
+		};
+
+		xhr.send(JSON.stringify(formData));
+	});
 };
--- src/mail.js
+++ src/mail.js
@@ -1,19 +1,19 @@
 const nodemailer = require("nodemailer");
 
 if (!process.env.MAIL_USER || !process.env.MAIL_PASS) {
-  throw new Error("Credenciais pendentes");
+	throw new Error("Credenciais pendentes");
 }
 
 const {
-  MAIL_HOST: host,
-  MAIL_PORT: port,
-  MAIL_USER: user,
-  MAIL_PASS: pass,
+	MAIL_HOST: host,
+	MAIL_PORT: port,
+	MAIL_USER: user,
+	MAIL_PASS: pass,
 } = process.env;
 
 module.exports = nodemailer.createTransport({
-  host,
-  port,
-  secure: true,
-  auth: { user, pass },
+	host,
+	port,
+	secure: true,
+	auth: { user, pass },
 });
